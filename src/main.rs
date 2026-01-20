4da0e63a2472bf51eaf16eab5151cc68fb94d443
Ron Fox <fox@frib.msu.edu>
2026-01-20 10:05:43 -0500

    Define the program usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
+//
+// Usage:
+//    trace-defenestrator in-uri outfile
+// See fn usage below.
 fn main() {
-    println!("Hello, world!");
+    usage();
+}
+
+fn usage() {
+    eprintln!("Usage:");
+    eprintln!("   trace-defenestrator in-uri outfile");
+    eprintln!("Where");
+    eprintln!("   in-uri - is a URI for the data source.");
+    eprintln!("            tcp://host/ring is supported for live data");
+    eprintln!("   outfile - Is the output file path. Note that");
+    eprintln!("           the special value '-' means stdout which,");
+    eprintln!("          combined with stdintoring supports contributing to a ringbuffer");
+
 }
