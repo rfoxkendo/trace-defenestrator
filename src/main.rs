c3d8f4d45e2cf25a34ba7a68536bb21d34a97d23
Ron Fox <fox@frib.msu.edu>
2026-01-21 07:25:50 -0500

    Initial coding...
    Handle the input and output specs.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,58 @@
 use std::env;
+use std::process::exit;
+use std::io;
+use std::io::Write;
+use std::fs::*;
 
+use frib_datasource;
 //
 // Usage:
 //    trace-defenestrator in-uri outfile
 // See fn usage below.
 fn main() {
-    usage();
+    let argv : Vec<String> = env::args().collect();
+    if argv.len() != 3 {   // Program path is included.
+        eprintln!("Incorrect number of command line arguments\n");
+        usage();
+        exit(-1);
+    }
+
+    let in_uri = argv[1].clone();
+    let outpath = argv[2].clone();
+
+    // Make the ring source.
+
+    let mut src = 
+    match frib_datasource::data_source_factory(&in_uri) {
+        Err(e) => {
+            eprintln!("Could not create the event source from the URI {}: {}", in_uri, e);
+            usage();
+            exit(-1);
+        },
+        Ok(src) => {
+            src
+        }
+    };
+
+    // Open the output file or stdout.:
+
+    let sink : Box<dyn Write> = if outpath == "-" {
+        Box::new(io::stdout())
+    } else {
+        let out = File::create(&outpath);
+        match out {
+            Err(e) => {
+                eprintln!("Could not open the output file {} : {}", outpath, e);
+                usage();
+                exit(-1);
+            },
+            Ok(f) =>  {
+                Box::new(f)
+            }
+        }
+        
+    };
+
 }
 
 fn usage() {
